fb4522db8ef7711538ae7ff2c2efa1fb90697cd3
GILGAMESH605 <159384299+GILGAMESH605@users.noreply.github.com>
2025-11-01 20:41:36 -0400

    Update exports and version in __init__.py
    Added motivational function to exports and updated version.

--- moodsmith/__init__.py
+++ moodsmith/__init__.py
@@ -1,6 +1,6 @@
 '''Exports functions we want users to import'''
 
-from .core import positive
+from .core import positive, motivational
 
 __all__ = ["positive"]  # functions that get imported
-__version__ = "0.0.1"  # packages current version
+__version__ = "0.0.2"  # packages current version
