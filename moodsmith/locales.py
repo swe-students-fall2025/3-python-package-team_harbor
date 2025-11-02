5094fce9318c10d0c91641076d35037ff939efe3
ct-04 <ct3551@nyu.edu>
2025-11-02 18:06:00 +0000

    Fix pylint issues

--- moodsmith/locales.py
+++ moodsmith/locales.py
@@ -1,17 +1,31 @@
-'''holds messages templates'''
+"""Holds message templates for positive_quote()."""
 
-# Message templates for the positive_quote() function.
 POSITIVE_TEMPLATES = {
-    "es" : ["Steve Jobs dijo que una computadora enseña a todos a pensar, y de hecho estás pensando{punct}",
-            "Estás haciendo un trabajo excelente{punct}",
-            "Los pequeños pasos se suman, y sin duda tú estás sumando arriba{punct}",
-            "Me encanta el progreso{punct} - Sigue adelante",],
-    "en" : ["Steve Jobs said a computer teaches everyone to think , and you are indeed thinking{punct}",
-            "You are doing a great job{punct}",
-            "Tiny steps add up, and you are certainly adding up{punct}",
-            "Love the progess{punct} - Keep going",], 
-    "fr" : ["Steve Jobs disait qu'un ordinateur apprend à chacun à penser, et vous êtes effectivement en train de penser{punct}",
-            "Vous faites un excellent travail{punct}",
-            "Les petits pas s'accumulent, et vous y contribuez certainement en haut{punct}",
-            "J'adore les progrès{punct} - continuer toujours",],  
+    "es": [
+        (
+            "Steve Jobs dijo que una computadora enseña a todos a pensar, "
+            "y de hecho estás pensando{punct}"
+        ),
+        "Estás haciendo un trabajo excelente{punct}",
+        "Los pequeños pasos se suman, y sin duda tú estás sumando{punct}",
+        "Me encanta el progreso{punct} - Sigue adelante",
+    ],
+    "en": [
+        (
+            "Steve Jobs said a computer teaches everyone to think, "
+            "and you are indeed thinking{punct}"
+        ),
+        "You are doing a great job{punct}",
+        "Tiny steps add up, and you are certainly adding{punct}",
+        "Love the progress{punct} - Keep going",
+    ],
+    "fr": [
+        (
+            "Steve Jobs disait qu'un ordinateur apprend à chacun à penser, "
+            "et vous êtes effectivement en train de penser{punct}"
+        ),
+        "Vous faites un excellent travail{punct}",
+        "Les petits pas s'accumulent, et vous y contribuez certainement{punct}",
+        "J'adore les progrès{punct} - continuer toujours",
+    ],
 }
